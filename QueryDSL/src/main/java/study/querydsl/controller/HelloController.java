94f20f9de21a38c18b0848603d14648418fa4d1e
h-1-y <h-1-y@naver.com>
2024-10-31 17:31:51 +0900

    QueryDSL Repository

--- QueryDSL/src/main/java/study/querydsl/controller/HelloController.java
+++ QueryDSL/src/main/java/study/querydsl/controller/HelloController.java
@@ -1,4 +1,4 @@
-package study.querydsl;
+package study.querydsl.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
